3a786b81782edd1700caf77161e2126d0ab5b3e7
Olexandr Lisovyi <microfox141120@outlook.com>
2023-09-16 20:46:29 +0300

    final

--- js/script.js
+++ js/script.js
@@ -7,14 +7,14 @@ let searchButton = document.querySelector(".search-button");
 let searchIcon = document.querySelector(".search-button svg");
 let errorBlock = document.querySelector(".error");
 let defaultCard =
-  '<div class="card inline-flex self-center"><div class="flex-col flex">  <p id="hour">3PM</p>  <h1 id="temperature" class="font-bold">   28 °C  </h1></div></div>';
+  '<div class="card inline-flex self-center"><div class="flex-col flex">  <p id="hour">3PM</p>  <h1 id="temperature" class="font-bold">   28 °C  </h1> <p id="weather">Scatered Clouds</p></div></div>';
 
 let activeCard =
-  '<div class="card active-card inline-flex"><div>  <p id="hour" class="text-[12px]">3PM</p>  <h1 id="temperature">28 °C</h1>  <p id="feels-like">Feels like 30°C</p></div></div>';
+  '<div class="card active-card inline-flex self-center"><div class="flex-col flex">  <p id="hour" class="text-[12px]">3PM</p>  <h1 id="temperature" class="font-bold">28 °C</h1>  <p id="weather">Scatered Clouds</p></div></div>';
 let cardsBlock = document.querySelector(".cards-block");
 
 // DATA
-let weather = document.querySelector("#weather");
+let currWeather = document.querySelector("#curr-weather");
 let city = document.querySelector("#city");
 let currentTemperature = document.querySelector("#current-temperature");
 let day = document.querySelector("#day");
@@ -25,30 +25,31 @@ let windSpeed = document.querySelector("#wind-speed");
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 // SERVER
-updatePage();
+updateCards();
 
-async function getData(city) {
-  try {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?units=metric&q=${city}&appid=a176d1906d2286711687fa945d96c7b8`
-    );
-    if (!response.ok) {
-      throw new Error(`Not Found`);
+async function getData(cityName = city.textContent.trim()) {
+  return new Promise(async (resolve) => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?units=metric&q=${cityName}&appid=a176d1906d2286711687fa945d96c7b8`
+      );
+      if (!response.ok) {
+        throw new Error(`Not Found`);
+      }
+      const data = await response.json();
+      resolve(data);
+    } catch (error) {
+      errorBlock.classList.add("show");
     }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    errorBlock.classList.add("show");
-  }
+  });
 }
 
-function updatePage(cityName = city.textContent.trim()) {
+function updateCards(cityName = city.textContent.trim()) {
   let listSize;
   getData(cityName).then((data) => {
     if (!data) throw new Error("Invalid name");
 
-    console.log(data);
-    listSize = data.list.length;
+    listSize = 8;
     while (cardsBlock.firstChild) {
       cardsBlock.removeChild(cardsBlock.firstChild);
     }
@@ -61,17 +62,16 @@ function updatePage(cityName = city.textContent.trim()) {
     }
     let hours = document.querySelectorAll("#hour");
     let temperatures = document.querySelectorAll("#temperature");
-    let feelsLike = document.querySelector("#feels-like");
-    let cards = document.querySelectorAll(".card");
+    let weather = document.querySelectorAll("#weather");
     for (let i = 0; i < listSize; i++) {
       hours[i].textContent = data.list[i].dt_txt.split(" ")[1].slice(0, 5);
       temperatures[i].textContent = Math.round(data.list[i].main.temp) + " °C";
+      weather[i].textContent = data.list[i].weather[0].description;
     }
+    let cards = document.querySelectorAll(".card");
     let activeCardIndex = [...cards].findIndex((i) => i.classList.contains("active-card"));
-
-    feelsLike.textContent = `Feels like: ${Math.round(data.list[0].main.feels_like)} °C `;
-    weather.textContent = data.list[0].weather[0].description;
-    city.textContent = cityName;
+    currWeather.textContent = data.list[activeCardIndex].weather[0].description;
+    city.textContent = city.textContent.trim();
     currentTemperature.textContent = temperatures[0].textContent;
     day.textContent =
       days[new Date(data.list[activeCardIndex].dt_txt.split(" ")[activeCardIndex]).getDay()];
@@ -81,27 +81,13 @@ function updatePage(cityName = city.textContent.trim()) {
     windSpeed.textContent = data.list[activeCardIndex].wind.speed + " km/h";
   });
 }
+
 // PRELOADER
 window.onload = function () {
   preloader.classList.remove("show");
 };
 
 // INTERFACE
-searchInput.addEventListener("input", function (e) {
-  searchButton.classList.add("toBlack");
-  searchIcon.querySelector("path").classList.add("toBlack");
-  if (searchInput.value == "") {
-    searchButton.classList.remove("toBlack");
-    searchIcon.querySelector("path").classList.remove("toBlack");
-  }
-});
-
-searchButton.addEventListener("click", function (e) {
-  e.preventDefault();
-  if (searchInput.value != "") {
-    if (!updatePage(searchInput.value)) errorBlock.classList.remove("show");
-  }
-});
 
 changeLocation.addEventListener("click", function (e) {
   e.preventDefault();
@@ -115,4 +101,29 @@ changeLocation.addEventListener("click", function (e) {
   searchBlock.classList.add("show");
 });
 
+searchInput.addEventListener("input", function (e) {
+  searchButton.classList.add("toBlack");
+  searchIcon.querySelector("path").classList.add("toBlack");
+  if (searchInput.value == "") {
+    searchButton.classList.remove("toBlack");
+    searchIcon.querySelector("path").classList.remove("toBlack");
+  }
+});
+
+searchButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  getCityInfo();
+});
+
+searchInput.addEventListener("keydown", function (e) {
+  if (e.key === "Enter" && searchBlock.classList.contains("show")) {
+    getCityInfo();
+  }
+});
+
+function getCityInfo() {
+  if (searchInput.value != "") {
+    if (!updateCards(searchInput.value)) errorBlock.classList.remove("show");
+  }
+}
 //cookie, multilang, slider
