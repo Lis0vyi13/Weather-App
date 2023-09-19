9f63e41af24163a1533819e679ec4a90fae0b914
Olexandr Lisovyi <microfox141120@outlook.com>
2023-09-19 23:33:49 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // ELEMENTS
+// 15 + 32
 let preloader = document.querySelector("#loaderContainer");
 let changeLocation = document.querySelector("#change-location");
 let searchBlock = document.querySelector(".search");
@@ -7,14 +8,15 @@ let searchButton = document.querySelector(".search-button");
 let searchIcon = document.querySelector(".search-button svg");
 let errorBlock = document.querySelector(".error");
 let defaultCard =
-  '<div class="card inline-flex self-center"><div class="flex-col flex">  <p id="hour">3PM</p>  <h1 id="temperature" class="font-bold">   28 °C  </h1> <p id="weather">Scatered Clouds</p></div></div>';
+  '<div class="card inline-flex self-center"><div class="flex-col flex">  <p id="hour">3PM</p>  <h1 id="temperature" class="font-bold"> 28 °C</h1> <p id="feels-like">Feels like 30°C</p></div></div>';
 
 let activeCard =
-  '<div class="card active-card inline-flex self-center"><div class="flex-col flex">  <p id="hour" class="text-[12px]">3PM</p>  <h1 id="temperature" class="font-bold">28 °C</h1>  <p id="weather">Scatered Clouds</p></div></div>';
+  '<div class="card active-card inline-flex"><div>  <p id="hour" class="text-[12px]">3PM</p>  <h1 id="temperature">28 °C</h1>  <p id="feels-like">Feels like 30°C</p></div></div>';
 let cardsBlock = document.querySelector(".cards-block");
+let buttons = document.querySelectorAll(".buttons");
 
 // DATA
-let currWeather = document.querySelector("#curr-weather");
+let weather = document.querySelector("#weather");
 let city = document.querySelector("#city");
 let currentTemperature = document.querySelector("#current-temperature");
 let day = document.querySelector("#day");
@@ -24,106 +26,193 @@ let rainChance = document.querySelector("#rain-chance");
 let windSpeed = document.querySelector("#wind-speed");
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
+document.addEventListener("DOMContentLoaded", function () {
+  updatePage();
+  updateData();
+});
 // SERVER
-updateCards();
-
-async function getData(cityName = city.textContent.trim()) {
-  return new Promise(async (resolve) => {
-    try {
-      const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?units=metric&q=${cityName}&appid=a176d1906d2286711687fa945d96c7b8`
-      );
-      if (!response.ok) {
-        throw new Error(`Not Found`);
-      }
-      const data = await response.json();
-      resolve(data);
-    } catch (error) {
-      errorBlock.classList.add("show");
+async function getData(cityName) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?units=metric&q=${cityName}&appid=a176d1906d2286711687fa945d96c7b8`
+    );
+    if (!response.ok) {
+      throw new Error(`Not Found`);
     }
-  });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    errorBlock.classList.add("show");
+    return null; // Возвращаем null, чтобы указать на ошибку
+  }
 }
 
-function updateCards(cityName = city.textContent.trim()) {
-  let listSize;
-  getData(cityName).then((data) => {
-    if (!data) throw new Error("Invalid name");
+async function updatePage(cityName = city.textContent.trim()) {
+  const data = await getData(cityName);
+  if (!data) {
+    console.error("Invalid data");
+    return;
+  }
 
-    listSize = 8;
-    while (cardsBlock.firstChild) {
-      cardsBlock.removeChild(cardsBlock.firstChild);
+  console.log(data);
+
+  while (cardsBlock.firstChild) {
+    cardsBlock.removeChild(cardsBlock.firstChild);
+  }
+  for (let i = 0; i < data.list.length; i++) {
+    if (i == 0) {
+      cardsBlock.insertAdjacentHTML("afterbegin", activeCard);
+    } else {
+      cardsBlock.insertAdjacentHTML("beforeend", defaultCard);
     }
-    for (let i = 0; i < listSize; i++) {
-      if (i == 0) {
-        cardsBlock.insertAdjacentHTML("afterbegin", activeCard);
+  }
+}
+
+async function updateData(cityName = city.textContent.trim()) {
+  const data = await getData(cityName);
+  if (!data) {
+    console.error("Invalid data");
+    return;
+  }
+
+  let hours = document.querySelectorAll("#hour");
+  let temperatures = document.querySelectorAll("#temperature");
+  let feelsLike = document.querySelectorAll("#feels-like");
+
+  let cardLength = data.list.length;
+  let cards = document.querySelectorAll(".card");
+  for (let i = 0; i < cardLength; i++) {
+    if (i > 0) {
+      feelsLike[i].style.display = "none";
+    }
+    hours[i].textContent = data.list[i].dt_txt.split(" ")[1].slice(0, 5);
+    temperatures[i].textContent = Math.round(data.list[i].main.temp) + " °C";
+    feelsLike[i].textContent = `Feels like: ${Math.round(data.list[i].main.feels_like)} °C `;
+  }
+
+  let activeCardIndex = [...cards].findIndex((i) => i.classList.contains("active-card"));
+  weather.textContent = data.list[0].weather[0].description;
+  city.textContent = cityName;
+  currentTemperature.textContent = temperatures[0].textContent;
+  day.textContent =
+    days[new Date(data.list[activeCardIndex].dt_txt.split(" ")[activeCardIndex]).getDay()];
+  humidity.textContent = data.list[activeCardIndex].main.humidity + "%";
+  airPressure.textContent = data.list[activeCardIndex].main.pressure + " PS";
+  rainChance.textContent = Math.round(data.list[activeCardIndex].pop * 100) + "%";
+  windSpeed.textContent = data.list[activeCardIndex].wind.speed + " km/h";
+}
+
+let count = 0;
+let value = 0;
+buttons.forEach(function (button) {
+  button.addEventListener("click", async function (e) {
+    const data = await getData(city.textContent.trim());
+    const cards = document.querySelectorAll(".card");
+    const activeCardIndex = [...cards].findIndex((i) => i.classList.contains("active-card"));
+    const cardsBlock = document.querySelector(".cards-block");
+    if (window.innerWidth >= 670) {
+      value = 143;
+    } else if (window.innerWidth >= 440) {
+      value = 132;
+    } else {
+      value = 116;
+    }
+
+    if (e.target == document.querySelector("#button-left")) {
+      if (activeCardIndex == 0) return;
+      count += value;
+      toggleActiveCard(cards, activeCardIndex, activeCardIndex - 1, data);
+    } else if (e.target == document.querySelector("#button-right")) {
+      count -= value;
+
+      if (activeCardIndex == 32) {
+        count = 0;
+        toggleActiveCard(cards, activeCardIndex, 0, data);
       } else {
-        cardsBlock.insertAdjacentHTML("beforeend", defaultCard);
+        toggleActiveCard(cards, activeCardIndex, activeCardIndex + 1, data);
       }
     }
-    let hours = document.querySelectorAll("#hour");
-    let temperatures = document.querySelectorAll("#temperature");
-    let weather = document.querySelectorAll("#weather");
-    for (let i = 0; i < listSize; i++) {
-      hours[i].textContent = data.list[i].dt_txt.split(" ")[1].slice(0, 5);
-      temperatures[i].textContent = Math.round(data.list[i].main.temp) + " °C";
-      weather[i].textContent = data.list[i].weather[0].description;
-    }
-    let cards = document.querySelectorAll(".card");
-    let activeCardIndex = [...cards].findIndex((i) => i.classList.contains("active-card"));
-    currWeather.textContent = data.list[activeCardIndex].weather[0].description;
-    city.textContent = city.textContent.trim();
-    currentTemperature.textContent = temperatures[0].textContent;
-    day.textContent =
-      days[new Date(data.list[activeCardIndex].dt_txt.split(" ")[activeCardIndex]).getDay()];
-    humidity.textContent = data.list[activeCardIndex].main.humidity + "%";
-    airPressure.textContent = data.list[activeCardIndex].main.pressure + " PS";
-    rainChance.textContent = Math.round(data.list[activeCardIndex].pop * 100) + "%";
-    windSpeed.textContent = data.list[activeCardIndex].wind.speed + " km/h";
+
+    cardsBlock.style.left = count + "px";
   });
+});
+
+function toggleActiveCard(cards, currentIndex, newIndex) {
+  cards[currentIndex].classList.toggle("active-card");
+
+  cards[currentIndex].querySelector("#feels-like").style.display = "none";
+
+  cards[newIndex].classList.toggle("active-card");
+  cards[newIndex].querySelector("#feels-like").style.display = "block";
 }
 
 // PRELOADER
-window.onload = function () {
-  preloader.classList.remove("show");
-};
+window.onload = () =>
+  setTimeout(() => {
+    preloader.classList.remove("show");
+  }, 200);
 
 // INTERFACE
-
-changeLocation.addEventListener("click", function (e) {
+changeLocation.addEventListener("click", (e) => {
   e.preventDefault();
   if (errorBlock.classList.contains("show")) {
     errorBlock.classList.remove("show");
   }
-  if (searchBlock.classList.contains("show")) {
-    searchBlock.classList.remove("show");
-    return;
-  }
-  searchBlock.classList.add("show");
+  searchBlock.classList.toggle("show");
 });
 
-searchInput.addEventListener("input", function (e) {
-  searchButton.classList.add("toBlack");
-  searchIcon.querySelector("path").classList.add("toBlack");
-  if (searchInput.value == "") {
-    searchButton.classList.remove("toBlack");
-    searchIcon.querySelector("path").classList.remove("toBlack");
-  }
+searchInput.addEventListener("input", (e) => {
+  const hasValue = searchInput.value !== "";
+  searchButton.classList.toggle("toBlack", hasValue);
+  searchIcon.querySelector("path").classList.toggle("toBlack", hasValue);
 });
 
-searchButton.addEventListener("click", function (e) {
+searchButton.addEventListener("click", async (e) => {
   e.preventDefault();
-  getCityInfo();
+  if (searchInput.value !== "") {
+    preloader.classList.add("show");
+    const result = await loadData(searchInput.value);
+
+    if (result) {
+      getCityInfo();
+    } else {
+      errorBlock.classList.remove("show");
+    }
+    setTimeout(() => {
+      preloader.classList.toggle("show");
+    }, 200);
+  }
 });
 
-searchInput.addEventListener("keydown", function (e) {
-  if (e.key === "Enter" && searchBlock.classList.contains("show")) {
-    getCityInfo();
+async function loadData(cityName) {
+  try {
+    const data = await getData(cityName);
+    return data !== null;
+  } catch (error) {
+    return false;
+  }
+}
+
+searchInput.addEventListener("keydown", async (e) => {
+  if (e.key === "Enter") {
+    preloader.classList.add("show");
+    const result = await loadData(searchInput.value);
+
+    if (result) {
+      getCityInfo();
+    } else {
+      errorBlock.classList.remove("show");
+    }
+    setTimeout(() => {
+      preloader.classList.toggle("show");
+    }, 200);
   }
 });
 
 function getCityInfo() {
-  if (searchInput.value != "") {
-    if (!updateCards(searchInput.value)) errorBlock.classList.remove("show");
+  if (searchInput.value !== "") {
+    if (!updatePage(searchInput.value) && !updateData(searchInput.value)) {
+      errorBlock.classList.remove("show");
+    }
   }
 }
-//cookie, multilang, slider
